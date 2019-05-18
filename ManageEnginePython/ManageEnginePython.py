76fe0f88c27f2a32b79242ab773b22226ae8c382
jane.lieberman@mau.com <jane.lieberman@mau.com>
2019-05-18 16:12:17 -0400

    planning commentary

--- ManageEnginePython/ManageEnginePython.py
+++ ManageEnginePython/ManageEnginePython.py
@@ -20,4 +20,17 @@ while 1:
     print(row.FIRST_NAME)#calls something from the select statement record
 cnxn.close()
 
+#this will be a background retrieval to print to somewhere else with
+
+#given a set of data (retrieved from this manner) how do I next want to display it in an editable form?
+# then how do I want to save that form to be pulled again by this system and updated by a fresh query run?
+# do i want the user interface to have a button to repopulate the timesheet - then look up table values? 
+# this seems programmable. I would need to store the time entered as a record identified by the task
+# then when pulling again get the last saved data for each of the tasks by task #
+# this can be a simple txt file stored in the background
+# i would want to do cleanup by overwriting the file.
+# it would eventually have the last entered value by task # for all tasks ever but that's ok since
+# the text file won't be large
+# how do i want to make the form? and how do i want people to have to call it?  this is trickier
+
 
