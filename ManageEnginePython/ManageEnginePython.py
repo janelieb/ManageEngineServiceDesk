405c76316286f5569cc41290244d47a029242706
jane.lieberman@mau.com <jane.lieberman@mau.com>
2019-05-18 15:57:33 -0400

    Created SQL Connection from Python using pyodbc library and windows authentication

--- ManageEnginePython/ManageEnginePython.py
+++ ManageEnginePython/ManageEnginePython.py
@@ -1,1 +1,23 @@
 
+
+# need this to connect to SQL Server
+import pyodbc
+
+conn_str = (
+    r'Driver={SQL Server};'
+    r'Server=mau-sql01;'
+    r'Database=ServiceDesk;'
+    r'Trusted_Connection=yes;'
+    )
+cnxn = pyodbc.connect(conn_str)
+cursor = cnxn.cursor()
+cursor.execute("Select * from AaaUser where FIRST_NAME like 'Jane%'")
+
+while 1:
+    row = cursor.fetchone()
+    if not row:
+        break
+    print(row.FIRST_NAME)#calls something from the select statement record
+cnxn.close()
+
+
