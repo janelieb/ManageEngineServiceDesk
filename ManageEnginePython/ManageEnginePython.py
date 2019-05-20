1e8acfe61fbe8f4186fb2dc591951b85e908db64
jane.lieberman@mau.com <jane.lieberman@mau.com>
2019-05-20 15:36:28 -0400

    Added hyperlink logic and edited query to show the parent object type

--- ManageEnginePython/ManageEnginePython.py
+++ ManageEnginePython/ManageEnginePython.py
@@ -3,36 +3,63 @@
 # need this to connect to SQL Server
 import pyodbc
 import pandas
+#need to import a gui library (kivas?)
+#need to import a code freeze library (pyinstaller)
 
-
+#program variables
+#Technician variable
+Tech = "Jane Lieberman"
+#connection string. can change server / database
 conn_str = (
     r'Driver={SQL Server};'
     r'Server=mau-sql01;'
     r'Database=ServiceDesk;'
     r'Trusted_Connection=yes;'
     )
-
-cnxn = pyodbc.connect(conn_str)
-#cursor = cnxn.cursor()
-
 #query currently grabs all open jobs for all techs, and we can filter and sort after adding to dataframes
 QueryFile = open("Query.txt", encoding='utf-8')
 Query = QueryFile.read()
-
-data = pandas.read_sql(Query,cnxn)
-#cursor.execute(Query)
-
-#while 1:
-#    row = cursor.fetchone()
-#    if not row:
-#        break
-#    print(row.RequestID , '\t' , row.Technician)#calls something from the select statement record
-
-print(data[['RequestID','Technician']])
-
-cnxn.close()
+#hyperlink strings
+#workorder = Link 1 + ID + Link 2
+#project = Link 1 + ID
+#Task = Link 1 + ID + Link 2
+Workorder_Link_1 = "http://192.168.0.59:8080/WorkOrder.do?woMode=viewWO&woID="
+Workorder_Link_2 = "&&fromListView=true"
+Project_Link_1 = "http://192.168.0.59:8080/ProjectAction.do?submitaction=ViewProject&fromListView=true&projectid="
+Task_Link_1 = "http://192.168.0.59:8080/TaskDefAction.do?submitaction=viewTask&TASKID="
+Task_Link_2 = "&from=project&fromListView=true"
+Problem_Link_1 = "http://192.168.0.59:8080/ProblemDetails.cc?PROBLEMID="
+Problem_Link_2 = "&fromListView=true"
 
 #this will be a background retrieval to print to somewhere else with
+cnxn = pyodbc.connect(conn_str)
+data = pandas.read_sql(Query,cnxn)
+#filter technician as this isn't done in the query to keep the variable passing at closer-to-the top layer
+DataByTech = data[data.Technician==Tech]
+#Creating hyperlink logic
+DataByTech['Link_1']=Workorder_Link_1
+DataByTech['Link_2']=Workorder_Link_2
+DataByTech['ParentLink_1']=""
+DataByTech['ParentLink_2']=""
+DataByTech.loc[DataByTech.ParentType=='WorkOrder','ParentLink_1']=Workorder_Link_1
+DataByTech.loc[DataByTech.ParentType=='WorkOrder','ParentLink_2']=Workorder_Link_2
+DataByTech.loc[DataByTech.RequestType=='Task','Link_1']=Task_Link_1
+DataByTech.loc[DataByTech.RequestType=='Task','Link_2']=Task_Link_2
+DataByTech.loc[DataByTech.RequestType=='Problem','Link_1']=Problem_Link_1
+DataByTech.loc[DataByTech.RequestType=='Problem','Link_2']=Problem_Link_2
+DataByTech.loc[DataByTech.ParentType=='Project','ParentLink_1']=Project_Link_1
+DataByTech.loc[DataByTech.ParentType=='Project','ParentLink_2']=""
+DataByTech.loc[DataByTech.RequestType=='Project','Link_1']=Project_Link_1
+DataByTech.loc[DataByTech.RequestType=='Project','Link_2']=""
+DataByTech['Link'] = DataByTech['Link_1']+DataByTech['RequestID'].map(str)+DataByTech['Link_2']
+DataByTech['ParentLink'] = DataByTech['ParentLink_1']+DataByTech['ParentID'].map(str)+DataByTech['ParentLink_2']
+#need to put together hyperlinks from this data
+print(DataByTech.filter(items=['RequestType','RequestID','ParentType','ParentID','Technician','Link','ParentLink']))
+
+#close files
+QueryFile.close()
+cnxn.close()
+
 
 #given a set of data (retrieved from this manner) how do I next want to display it in an editable form?
 # then how do I want to save that form to be pulled again by this system and updated by a fresh query run?
